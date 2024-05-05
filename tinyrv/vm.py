c8f7f0f7255dc641f95ae01350f044f6c3f7888e
stefan <mail@s-holst.de>
2024-05-05 20:13:02 +0900

    a semihosting vm

--- tinyrv/vm.py
+++ tinyrv/vm.py
@@ -1,6 +1,7 @@
 import sys, struct, time, dataclasses, argparse
 
 import lief
+import dataclasses_struct as dcs
 
 from tinyrv import sim, zext
 
@@ -16,45 +17,39 @@ def load_elf(vm, elf, trace=False):
     vm.pc = elf.entrypoint
     if trace: print(f'ELF entry point: {hex(vm.pc)}')
 
-@dataclasses.dataclass
-class kernel_stat:
-    st_dev:     int = 24    # unsigned long long
-    st_ino:     int = 3     # unsigned long long
-    st_mode:    int = 8592  # unsigned int
-    st_nlink:   int = 1     # unsigned int
-    st_uid:     int = 1000  # unsigned int
-    st_gid:     int = 5     # unsigned int
-    st_rdev:    int = 34816 # unsigned long long
-    __pad1:     int = 0     # unsigned long long
-    st_size:    int = 0     # long long
-    st_blksize: int = 1024  # int
-    __pad2:     int = 0     # int
-    st_blocks:  int = 0     # long long
-    st_atim:    int = int(time.monotonic())  # struct timespec
-    st_mtim:    int = int(time.monotonic())  # struct timespec
-    st_ctim:    int = int(time.monotonic())  # struct timespec
-    __glibc_reserved1 = 0 # int
-    __glibc_reserved2 = 0 # int
+def pack_args(args, xlen=32, sp=0x80000000):
+    offsets, strings = [0], []
+    for a in args:
+        l = len(a)+1
+        strings.append(struct.pack(f'{l}s', a.encode()))
+        offsets.append(offsets[-1]+l)
+    ptr_bytes = 4 if xlen == 32 else 8
+    ptr_fmt = 'I' if xlen == 32 else 'Q'
+    padding = ptr_bytes - (offsets[-1] % ptr_bytes)
+    sp -= len(strings)*ptr_bytes + padding + offsets[-1]
+    ptrs = []
+    for o in offsets[:-1]: ptrs.append(struct.pack(ptr_fmt, sp+len(strings)*ptr_bytes+o+padding))
+    return sp-ptr_bytes, struct.pack(ptr_fmt, len(strings)) + b''.join(ptrs+([b'\0']*padding)+strings)
 
-    def bytes(self):
-        return struct.pack('QQIIIIQQqiiqIIIII',
-            self.st_dev    ,
-            self.st_ino    ,
-            self.st_mode   ,
-            self.st_nlink  ,
-            self.st_uid    ,
-            self.st_gid    ,
-            self.st_rdev   ,
-            self.__pad1    ,
-            self.st_size   ,
-            self.st_blksize,
-            self.__pad2    ,
-            self.st_blocks ,
-            self.st_atim   ,
-            self.st_mtim   ,
-            self.st_ctim   ,
-            self.__glibc_reserved1,
-            self.__glibc_reserved2)
+@dcs.dataclass()
+class kernel_stat2:
+    st_dev:     dcs.U64 = 24
+    st_ino:     dcs.U64 = 3
+    st_mode:    dcs.U32 = 8592
+    st_nlink:   dcs.U32 = 1
+    st_uid:     dcs.U32 = 1000
+    st_gid:     dcs.U32 = 5
+    st_rdev:    dcs.U64 = 34816
+    __pad1:     dcs.U64 = 0
+    st_size:    dcs.I64 = 0
+    st_blksize: dcs.I32 = 1024
+    __pad2:     dcs.I32 = 0
+    st_blocks:  dcs.I64 = 0
+    st_atim:    dcs.I32 = int(time.monotonic())  # struct timespec
+    st_mtim:    dcs.I32 = int(time.monotonic())  # struct timespec
+    st_ctim:    dcs.I32 = int(time.monotonic())  # struct timespec
+    __glibc_r1: dcs.I32 = 0
+    __glibc_r2: dcs.I32 = 0
 
 class linux(sim):
     def __init__(self, elf_file, args=[], trace=False):
@@ -68,28 +63,16 @@ class linux(sim):
         self.heap_end = heap_end_sym.value if heap_end_sym is not None else 0xa0000000
         self.keep_running = True
         self.exitcode = 0
-        self.x[self.sp], arg_data = self.pack_args(args) # puts argc and **argv on stack and init sp
+        self.x[self.sp], arg_data = pack_args(args, self.xlen)  # puts argc and **argv on stack and init sp
         self.copy_in(self.x[self.sp], arg_data)
 
-    def pack_args(self, args, sp=0x7fffffff):
-        offsets, strings = [0], []
-        for a in args:
-            l = len(a)+1
-            strings.append(struct.pack(f'{l}s', a.encode()))
-            offsets.append(offsets[-1]+l)
-        ptr_bytes = 4 if self.xlen == 32 else 8
-        ptr_fmt = 'I' if self.xlen == 32 else 'Q'
-        sp -= len(strings)*ptr_bytes + offsets[-1] - 1
-        ptrs = []
-        for o in offsets[:-1]: ptrs.append(struct.pack(ptr_fmt, sp+len(strings)*ptr_bytes+o))
-        return sp-ptr_bytes, struct.pack(ptr_fmt, len(strings)) + b''.join(ptrs+strings)
-
     def _ecall(self, **_):
+        # helpful: https://jborza.com/post/2021-05-11-riscv-linux-syscalls/
         syscall_no = self.x[self.a7]
         if syscall_no == 80:  # fstat
             fd = self.x[self.a0]
             out_mem_ptr = self.x[self.a1]
-            self.copy_in(out_mem_ptr, kernel_stat().bytes())
+            self.copy_in(out_mem_ptr, kernel_stat2().pack())
             self.x[self.a0] = 0  # success
             self.pc += 4
             return
@@ -139,6 +122,73 @@ class linux(sim):
             current_time = zext(32, int(time.perf_counter()*1000))
             self.store('I', 0x10000000, current_time, notify=False)
         return self.keep_running
+
+class semihosting(sim):
+    def __init__(self, elf_file, args=[], trace=False):
+        elf = lief.parse(elf_file)
+        assert elf.header.machine_type == lief.ELF.ARCH.RISCV
+        super().__init__(xlen=32 if elf.header.identity_class == lief.ELF.ELF_CLASS.CLASS32 else 64, trap_misaligned=False)
+        self.elf_file, self.elf = elf_file, elf
+        self.trace = trace
+        load_elf(self, self.elf, trace=trace)
+        self.exitcode = 0
+        self.x[self.sp], arg_data = pack_args(args, self.xlen)  # puts argc and **argv on stack and init sp
+        self.copy_in(self.x[self.sp], arg_data)
+
+    def _ebreak(self, **_):
+        if self.load('I', self.op.addr-4, 0, notify=False) != 0x01f01013: return super()._ebreak()  # check slli zero,zero,0x1f before
+        if self.load('I', self.op.addr+4, 0, notify=False) != 0x40705013: return super()._ebreak()  # check srai zero,zero,0x7 after
+        # we have a semihost call
+        semihost_no = self.x[self.a0]
+        semihost_param = self.x[self.a1]
+        ptr_bytes = 4 if self.xlen == 32 else 8
+        ptr_fmt = 'I' if self.xlen == 32 else 'Q'
+        #print(f'semihost {hex(semihost_no)} {hex(semihost_param)} from {hex(self.op.addr)}')
+        if semihost_no == 1:  # open
+            file_ptr = self.load('I', self.x[self.a1], 0, notify=False)
+            s = []
+            while (c := self.load('B', file_ptr, 0, notify=False)) != 0:
+                s.append(chr(c))
+                file_ptr += 1
+            fname = ''.join(s)
+            mode = self.load(ptr_fmt, self.x[self.a1]+ptr_bytes, 0, notify=False)
+            #fnlen = self.load('I', self.x[self.a1]+ptr_bytes*2, 0, notify=False)
+            handle = {(':tt', 0): 1, (':tt', 4): 2, (':tt', 8): 3}.get((fname, mode), -1)  # r:stdin, w:stdout, a:stderr
+            #print(f'open {fname} mode {mode} -> {handle}')
+            self.x[self.a0] = handle
+            self.pc += 4
+        elif semihost_no == 0x0c:  # flen
+            handle = self.load(ptr_fmt, self.x[self.a1], 0, notify=False)
+            flen = 0
+            #print(f'flen {handle} -> {flen}')
+            self.x[self.a0] = flen
+            self.pc += 4
+        elif semihost_no == 0x09:  # istty
+            handle = self.load(ptr_fmt, self.x[self.a1], 0, notify=False)
+            #print(f'istty {handle} -> 1')
+            self.x[self.a0] = 1
+            self.pc += 4
+        elif semihost_no == 0x05:  # write
+            handle = self.load(ptr_fmt, self.x[self.a1], 0, notify=False)
+            ptr = self.load(ptr_fmt, self.x[self.a1]+ptr_bytes, 0, notify=False)
+            length = self.load(ptr_fmt, self.x[self.a1]+ptr_bytes*2, 0, notify=False)
+            s = self.copy_out(ptr, length).decode()
+            print(s, end='', flush=True)
+            #print(f'write {hex(ptr)} {length}')
+            self.x[self.a0] = 0
+            self.pc += 4
+        elif semihost_no == 0x02:  # close
+            handle = self.load(ptr_fmt, self.x[self.a1], 0, notify=False)
+            #print(f'close {handle} -> 0')
+            self.x[self.a0] = 0
+            self.pc += 4
+        elif semihost_no == 0x18:  # exit
+            reason = semihost_param
+            #print(f'exit {hex(reason)} -> 0')
+            self.x[self.a0] = 0
+            self.exitcode = semihost_param != 0x20026  # exit 1 if not a normal application exit.
+        else:
+            exit(0)
 
 class htif(sim):  # Berkeley Host-Target Interface (HTIF)
     def __init__(self, elf_file, args=[], trace=False):
@@ -190,6 +240,19 @@ def run_riscof_vm():
     end_signature = vm.elf.get_symbol('end_signature').value
     for addr in range(begin_signature, end_signature, 4):
         print(f'{vm.load("I",addr):08x}')
+    return vm.exitcode
+
+def run_semihosting_vm():
+    parser = argparse.ArgumentParser(
+                    prog='tinyrv-vm-semihosting',
+                    description='Runs the binary with semihosting.')
+    parser.add_argument('-t', '--trace', action='store_true')
+    parser.add_argument('-l', '--limit', type=int, default=0)
+    parser.add_argument('elf', type=argparse.FileType('rb'))
+    parser.add_argument('args', nargs='*')
+    args = parser.parse_args()
+    vm = semihosting(args.elf, [args.elf.name] + args.args, trace=args.trace)
+    vm.run(args.limit, trace=args.trace)
     return vm.exitcode
 
 def run_linux_vm():
